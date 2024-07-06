decbde2fc8ea0a3f4900e97fe240edd54d0884b9
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-07-06 09:12:02 +0100

    backgroundColor + onestTextUpdate + headerImage + Pageheader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Metadata } from 'next'
+import { OnestText } from '@/components/atoms/onest_text'
 
 export const metadata: Metadata = {
   title: 'Portfolio Enrique Suarez | Inicio',
@@ -8,7 +9,26 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline"> Home Page</h1>
+      <header>
+        <div>
+          <Image
+            src={
+              process.env.NEXT_PUBLIC_IMAGES_PATH +
+              '/public/images/header_image.avif'
+            }
+            alt="Enrique Suarez"
+            width={200}
+            height={200}
+          />
+          <OnestText
+            tag="h1"
+            text="Enrique Suarez"
+            style="bold"
+            fontSize="64px"
+            className="text-white"
+          />
+        </div>
+      </header>
     </main>
   )
 }
