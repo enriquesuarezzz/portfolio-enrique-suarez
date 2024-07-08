8f91b596f58f9856f7e23183707e734fdd4a82fd
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-07-08 08:58:33 +0100

    NavbarComponentUpdate + OnestTextUpdate + pageMinorUpdates + coloradded

--- src/components/molecules/navbar/navbar.tsx
+++ src/components/molecules/navbar/navbar.tsx
@@ -1,3 +1,37 @@
-export default function navbar() {
-  ;<div></div>
+import { OnestText } from '@/components/atoms/onest_text'
+import Link from 'next/link'
+
+export default function Navbar() {
+  const data = [
+    {
+      title: 'Experiencia',
+      href: '/experiencia',
+    },
+    {
+      title: 'Proyectos',
+      href: '/proyectos',
+    },
+    {
+      title: 'Sobre Mí',
+      href: '/sobremi',
+    },
+    {
+      title: 'Contacto',
+      href: '/contacto',
+    },
+  ]
+  return (
+    <nav className="fixed mx-auto flex w-full justify-center gap-3 pt-5 md:gap-5">
+      {data.map((item) => (
+        <Link href={item.href} key={item.title}>
+          <OnestText
+            text={item.title}
+            style="bold"
+            fontSize="18px"
+            className="hover:text-orange text-white"
+          />
+        </Link>
+      ))}
+    </nav>
+  )
 }
