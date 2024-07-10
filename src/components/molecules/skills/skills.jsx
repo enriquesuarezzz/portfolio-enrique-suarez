f67718468b85e9b0c76a6d7f2378ed5f0e9e226a
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-07-10 23:22:19 +0100

    skillsSectionCreate

---
+++ src/components/molecules/skills/skills.jsx
@@ -0,0 +1,17 @@
+import { OnestText } from '@/components/atoms/onest_text'
+export default function Skills() {
+  return (
+    <section>
+      <div>
+        <OnestText
+          text="Habilidades"
+          style="bold"
+          fontSize="32px"
+          className="text-white"
+        />
+      </div>
+      <div></div>
+      <div></div>
+    </section>
+  )
+}
