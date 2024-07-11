a482f94964506c4dd528602b722b8c5ba9b3196d
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-07-11 11:10:29 +0100

    commentsAdded

--- src/components/molecules/skills/skills.tsx
+++ src/components/molecules/skills/skills.tsx
@@ -3,6 +3,7 @@ import Tools from '@/components/atoms/svg/tools'
 export default function Skills() {
   return (
     <section className="pt-10 md:pt-20">
+      {/* section title */}
       <div className="flex items-center justify-center gap-4 pb-8 md:justify-start">
         <Tools />
         <OnestText
@@ -12,7 +13,9 @@ export default function Skills() {
           className="text-white"
         />
       </div>
+      {/* tecnologies */}
       <div className="grid grid-cols-2 gap-6">
+        {/* frontend */}
         <div className="h-64 w-full rounded-2xl bg-dark-blue/60">
           <OnestText
             text="FrontEnd"
@@ -21,6 +24,7 @@ export default function Skills() {
             className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-orange dark:text-transparent"
           />
         </div>
+        {/* backend */}
         <div className="h-64 w-full rounded-2xl bg-dark-blue/60">
           <OnestText
             text="BackEnd"
@@ -29,6 +33,7 @@ export default function Skills() {
             className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-orange dark:text-transparent"
           />
         </div>
+        {/* learning */}
         <div className="h-64 w-full rounded-2xl bg-dark-blue/60">
           <OnestText
             text="Aprendiendo"
@@ -37,6 +42,7 @@ export default function Skills() {
             className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-orange dark:text-transparent"
           />
         </div>
+        {/* tools */}
         <div className="h-64 w-full rounded-2xl bg-dark-blue/60">
           <OnestText
             text="Herramientas"
