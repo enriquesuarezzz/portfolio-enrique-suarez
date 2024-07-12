077f59de24022cb71e2f997b9707e05aace17e49
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-07-12 08:00:46 +0100

    skillsSectionUpdate

--- src/components/molecules/skills/skills.tsx
+++ src/components/molecules/skills/skills.tsx
@@ -16,40 +16,44 @@ export default function Skills() {
       {/* tecnologies */}
       <div className="grid grid-cols-2 gap-6">
         {/* frontend */}
-        <div className="h-64 w-full rounded-2xl bg-dark-blue/60">
+        <div>
           <OnestText
             text="FrontEnd"
             style="bold"
             fontSize="36px"
-            className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-orange dark:text-transparent"
+            className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-blue-500 dark:text-transparent"
           />
+          <div className="h-64 w-full rounded-2xl bg-dark-blue/60"></div>
         </div>
         {/* backend */}
-        <div className="h-64 w-full rounded-2xl bg-dark-blue/60">
+        <div>
           <OnestText
             text="BackEnd"
             style="bold"
             fontSize="36px"
-            className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-orange dark:text-transparent"
+            className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-purple-400 dark:text-transparent"
           />
+          <div className="h-64 w-full rounded-2xl bg-dark-blue/60"></div>
         </div>
         {/* learning */}
-        <div className="h-64 w-full rounded-2xl bg-dark-blue/60">
+        <div>
           <OnestText
             text="Aprendiendo"
             style="bold"
             fontSize="36px"
-            className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-orange dark:text-transparent"
+            className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-rose-950 dark:text-transparent"
           />
+          <div className="h-64 w-full rounded-2xl bg-dark-blue/60"></div>
         </div>
         {/* tools */}
-        <div className="h-64 w-full rounded-2xl bg-dark-blue/60">
+        <div>
           <OnestText
             text="Herramientas"
             style="bold"
             fontSize="36px"
-            className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-orange dark:text-transparent"
+            className="bg-gradient-to-t from-[#54fbff] to-[#00a4c9] bg-clip-text text-center text-transparent dark:from-[#d4b48f] dark:to-emerald-600 dark:text-transparent"
           />
+          <div className="h-64 w-full rounded-2xl bg-dark-blue/60"></div>
         </div>
       </div>
     </section>
