3c34998d1dbf772bdd4edbdae8e9cb1e5656e1d7
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-07-16 21:25:31 +0100

    ProyectsCardsComponentCreate

--- src/components/molecules/proyects/proyects.tsx
+++ src/components/molecules/proyects/proyects.tsx
@@ -4,6 +4,13 @@ import Image from 'next/image'
 import Button from '../button/button'
 
 export default function Proyects() {
+  const proyects = [
+    {
+      image: '/images/mumbai.avif',
+      title: 'Marketec 360',
+      description: 'Aplicacion web para el manejo de inventario',
+    },
+  ]
   return (
     <section className="pt-10 md:pt-20">
       {/* section title */}
@@ -19,36 +26,41 @@ export default function Proyects() {
       </div>
       {/* cards */}
       <div className="grid grid-cols-2 gap-20">
-        <div className="relative flex h-full max-h-[500px] max-w-[500px] flex-col items-center justify-center rounded-3xl border border-gray-400 bg-gradient-to-r from-gray-800 to-gray-900 shadow-2xl">
-          <Image
-            src={`${process.env.NEXT_PUBLIC_IMAGES_PATH + '/images/mumbai.avif'}`}
-            width={500}
-            height={300}
-            alt="Project"
-            className="h-auto w-full rounded-2xl object-cover px-10 pt-4"
-          />
-          <div className="px-6 py-4">
-            <OnestText
-              text="Marketec 360"
-              style="bold"
-              fontSize="18px"
-              className="pt-4 text-center text-2xl font-bold text-white"
+        {proyects.map(({ image, title, description }) => (
+          <div
+            key={title}
+            className="relative flex h-full max-h-[500px] max-w-[500px] flex-col items-center justify-center rounded-3xl border border-gray-400 bg-gradient-to-r from-gray-800 to-gray-900 shadow-2xl"
+          >
+            <Image
+              src={`${process.env.NEXT_PUBLIC_IMAGES_PATH + image}`}
+              width={500}
+              height={300}
+              alt="Project"
+              className="h-auto w-full rounded-2xl object-cover px-10 pt-4"
             />
-            <OnestText
-              text="proyecto de prueba creado con NextJS y TailwindCSS. Este proyecto es una demostración de las capacidades de estas tecnologías para construir interfaces de usuario modernas y receptivas."
-              style="bold"
-              fontSize="16px"
-              className="px-4 pt-2 text-center text-sm text-gray-300"
-            />
+            <div className="px-6 py-4">
+              <OnestText
+                text={title}
+                style="bold"
+                fontSize="18px"
+                className="pt-4 text-center text-2xl font-bold text-white"
+              />
+              <OnestText
+                text={description}
+                style="bold"
+                fontSize="16px"
+                className="px-4 pt-2 text-center text-sm text-gray-300"
+              />
+            </div>
+            <div className="mb-6 mt-4">
+              <Button
+                text="Ver proyecto"
+                href="https://marketec360.com/"
+                className="from-primary to-secondary transform rounded-full bg-gradient-to-r px-6 py-2 font-semibold text-white transition-transform hover:scale-105"
+              />
+            </div>
           </div>
-          <div className="mb-6 mt-4">
-            <Button
-              text="Ver proyecto"
-              href="https://marketec360.com/"
-              className="from-primary to-secondary transform rounded-full bg-gradient-to-r px-6 py-2 font-semibold text-white transition-transform hover:scale-105"
-            />
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   )
